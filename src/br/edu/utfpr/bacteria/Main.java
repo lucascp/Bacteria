7dc852de8338342f34cf0b1146b48c99221b4c6b
Luiz Ribeiro <luizribeiro@gmail.com>
2011-05-17 00:37:49 -0300

    Primeiro commit

---
+++ src/br/edu/utfpr/bacteria/Main.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.bacteria;
+
+import br.edu.utfpr.bacteria.ui.MainWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainWindow().setVisible(true);
+    }
+}
